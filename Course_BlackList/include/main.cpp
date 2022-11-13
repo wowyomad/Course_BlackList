b3c584255a03549c19cc11087354fdb4988433e1
wowyomad <vadimsun8837@gmail.com>
2022-11-13 19:30:16 +0300

    похуй2

--- Course_BlackList/include/main.cpp
+++ Course_BlackList/include/main.cpp
@@ -1,4 +1,4 @@
-#include "Setup.hpp"
+п»ї#include "Setup.hpp"
 #include "RNG.hpp"
 
 #include <iostream>
@@ -7,36 +7,30 @@
 #include <fcntl.h>
 #include <io.h>
 
-
 int main()
 {
 	/*SetConsoleCP(65001);
 	SetConsoleOutputCP(65001);
 	system("chcp 65001");
 	setlocale(LC_ALL, "ru_RU.utf8");
-	std::ios::sync_with_stdio(false);
+	;*/
+
+	/*std::ios::sync_with_stdio(false);
 	std::wcout.imbue(std::locale("ru_RU.utf8"));
 	std::wcin.imbue(std::locale("ru_RU.utf8"));*/
 
-	//std::cout.imbue(std::locale("ru_RU.utf8"));
-	//std::cin.imbue(std::locale("ru_RU.utf8"));
-	
+	//setlocale(LC_ALL, "Russian"); // Р·Р°РґР°С‘Рј СЂСѓСЃСЃРєРёР№ С‚РµРєСЃС‚
+	//system("chcp 1251");
+
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 	/*SetConsoleOutputCP(65001);*/
 
-	std::string aboba = "Сука блядскя";
+	std::string aboba = "СЃСѓРєР°";
 	std::string test;
 	std::cout << _getch() << _getch();
 	std::cin >> test;
-	std::cout << aboba << '\t' << test << '\n';/*
-	if (aboba == test)
-	{
-		std::wcout << "Верно";
-	}
-	else
-	{
-		std::cout << "Неверно";
-	}*/
+	std::cout << aboba << '\t';
+	std::cout << test << '\n';
 }
 
